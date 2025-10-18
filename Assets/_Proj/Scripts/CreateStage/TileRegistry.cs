0485f7043a2a929392132ac0731f7e6c7fc4efd8
HJK <ksturm960@gmail.com>
2025-10-19 01:52:29 +0900

    TileRegistry
    맵 생성에 사용되는 데이터 레지스트리 스크립트.
    데이터 정의.
    중앙집중식 저장소 -> 데이터 세트를 하나의 .asset으로 저장하도록.
    데이터 캐싱 및 조회.
    Addressables시스템 활용하면 더 좋을 듯 하여 수정 예정.

---
+++ Assets/_Proj/Scripts/CreateStage/TileRegistry.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(menuName = "TileRegistry")]
+public class TileRegistry : ScriptableObject
+{
+    [Serializable] 
+    public class TileBinding
+    {
+        public string code; // "G", "W", "P", "I", "B" ...
+        public GameObject prefab;
+        public bool blocking; // 벽 등
+        public bool water; // 물
+        public bool ice; // 빙판
+        public bool pit; // 구덩이
+        public bool swamp; // 늪
+    }
+
+    [Serializable]
+    public class EntityBinding
+    {
+        public string type; // "spawn", "goal", "box", "door", "bridge", ...
+        public GameObject prefab;
+        
+    }
+
+    public List<TileBinding> tiles;
+    public List<EntityBinding> entities;
+
+    Dictionary<string, TileBinding> tmap;
+    Dictionary<string, EntityBinding> emap;
+
+    public void Init()
+    {
+        if (tmap == null)
+        {
+            tmap = new();
+            foreach(var t in tiles)
+            {
+                if (!string.IsNullOrEmpty(t.code))
+                {
+                    tmap[t.code] = t;
+                }
+            }
+        }
+        if (emap == null)
+        {
+            emap = new();
+            foreach (var e in entities)
+            {
+                if (!string.IsNullOrEmpty(e.type))
+                {
+                    emap[e.type] = e;
+                }
+            }
+        }
+    }
+
+    public TileBinding GetTile(string code)
+    {
+        Init();
+        return tmap != null && tmap.TryGetValue(code, out var x) ? x : null;
+    }
+
+    public EntityBinding GetEntity(string type) { 
+        Init();
+        return emap != null && emap.TryGetValue(type, out var x) ? x : null;
+    }
+
+    public bool HasCode(string code)
+    {
+        Init(); 
+        return tmap != null && tmap.ContainsKey(code);
+    }
+
+    public bool HasEntity(string type)
+    {
+        Init();
+        return emap != null && emap.ContainsKey(type);
+    }
+}
