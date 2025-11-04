41ac3ae16904304fb35009fd70ecf438573e5790
JangWh <janghy95@naver.com>
2025-11-04 16:41:00 +0900

    GameSystem 인게임 UI, 데이터테이블 갱신
    1. 인게임 환경설정 버튼 생성
    2. 클릭시 다시하기, 나가기등 옵션 생성
    3. 기획에서 넘겨준 데이터테이블 갱신 - 프로필

---
+++ Assets/_Proj/Scripts/UI/Comon/StageUIManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StageUIManager : MonoBehaviour
+{
+    [Header("Button")]
+    public Button OptionOpenButton;
+    public Button OptionCloseButton;
+    public Button RetryButton;
+    public Button QuitButton;
+
+    [Header("OptionObject")]
+    public GameObject OptionImg;
+
+    void Awake()
+    {
+        OptionOpenButton.onClick.AddListener(OptionOpen);
+        OptionCloseButton.onClick.AddListener(OptionClose);
+        RetryButton.onClick.AddListener(Retry);
+        QuitButton.onClick.AddListener(Quit);
+        OptionImg.SetActive(false);
+    }
+
+    void OptionOpen()
+    {
+        OptionImg.SetActive(true);
+        OptionOpenButton.gameObject.SetActive(false);
+    }
+
+    void OptionClose()
+    {
+        OptionImg.SetActive(false);
+        OptionOpenButton.gameObject.SetActive(true);
+    }
+
+    void Retry()
+    {
+        print("다시하지롱");
+        //
+    }
+
+    void Quit()
+    {
+        print("나가지롱");
+    }
+}
