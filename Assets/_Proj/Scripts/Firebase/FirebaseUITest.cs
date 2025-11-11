504ee7385d8433720463605a9628e861e5749834
이강욱 <kangwook.yi@gmail.com>
2025-11-11 15:58:15 +0900

    [Firebase] : 익명 로그인 기능 테스트 정상 확인(테스트 씬에서 버튼 클릭으로 작동)

---
+++ Assets/_Proj/Scripts/Firebase/FirebaseUITest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseUITest : MonoBehaviour
+{
+    //public Button testLoginButton_Anonymous;
+
+
+
+    public async void OnAnonymousLoginButtonClick()
+    {
+        if (!FirebaseManager.Instance && !FirebaseManager.Instance.IsInitialized) return;
+        await FirebaseManager.Instance.SignInAnonymouslyTest((x) => Debug.Log($"파이어베이스 로그인 테스트: [성공]{x.UserId}로 로그인 성공"), x => Debug.LogWarning($"파이어베이스 로그인 테스트: [실패] - {x.Message}"));
+
+    }
+}
--- Assets/_Proj/Scripts/Managers/FirebaseManager.cs
+++ Assets/_Proj/Scripts/Managers/FirebaseManager.cs
@@ -51,6 +51,10 @@ public class FirebaseManager : MonoBehaviour
 
             //추가: 파이어베이스 인증 기능 활용을 위해 현재 App에서 Firebase Authentication 어플리케이션을 가져옵니다.
             Auth = FirebaseAuth.GetAuth(App);
+            IsInitialized = true;
+            Debug.Log($"[파이어베이스 인증]로컬에 남아있는 유저 아이디 : {Auth.CurrentUser.UserId}");
+            
+            
         }
         else
         {
@@ -176,8 +180,75 @@ public class FirebaseManager : MonoBehaviour
     }
 
 
-
-
     //Firebase Auth 관련 기능.
+    public async Task SignInAnonymouslyTest(Action<FirebaseUser> onSuccess, Action<FirebaseException> onFailure) => await SignInAnonymously(onSuccess, onFailure);
 
+
+
+    /// <summary>
+    /// 익명 로그인 기능.
+    /// </summary>
+    /// <param name="onSuccess">성공 시의 FirebaseUser를 매개변수로 삼아 실행시킬 콜백 함수</param>
+    /// <param name="onFailure">실패 시 catch되는 FirebaseException을 매개변수로 삼아 실행시킬 콜백 함수</param>
+    /// <returns></returns>
+    private async Task SignInAnonymously(Action<FirebaseUser> onSuccess, Action<FirebaseException> onFailure)
+    {
+        try
+        {
+            var result = await Auth.SignInAnonymouslyAsync();
+            //SaveUserData(result.User);
+            onSuccess?.Invoke(result.User);
+        }
+        catch (FirebaseException ex)
+        {
+            onFailure?.Invoke(ex);
+        }
+    }
+
+    [Obsolete("유저데이터 클래스 정의를 먼저 하고 사용해야 함. 정의 및 연결 끝나면 이 Obsolete는 지우고 정식으로 사용 예정.", false)]
+    /// <summary>
+    /// 유저 데이터 저장 기능.
+    /// </summary>
+    /// <param name="user">데이터를 저장할 FirebaseUser</param>
+    private void SaveUserData(FirebaseUser user)
+    {
+        if (user == null) return;
+
+        var userRef = DB.GetReference("users").Child(user.UserId);
+        var userData = new
+        {
+            uid = user.UserId,
+            email = user.Email,
+            isAnonymous = user.IsAnonymous,
+            displayName = user.DisplayName,
+            lastLogin = DateTime.UtcNow.ToString("o")
+        };
+
+        userRef.SetRawJsonValueAsync(JsonUtility.ToJson(userData));
+    }
+
+    /// <summary>
+    /// 로그인된 익명 유저의 인증 정보를 구글 계정과 연결하는 함수.
+    /// </summary>
+    /// <param name="idToken"></param>
+    /// <param name="accessToken"></param>
+    /// <param name="onSuccess"></param>
+    /// <param name="onFailure"></param>
+    /// <returns></returns>
+    public async Task LinkAnonymousToGoogle(string idToken, string accessToken, Action<FirebaseUser> onSuccess, Action<FirebaseException> onFailure)
+    {
+        //인증의 현재 유저가 익명 유저가 아닐 경우 리턴
+        if (!Auth.CurrentUser.IsAnonymous) return;
+
+
+        var credential = GoogleAuthProvider.GetCredential(idToken, accessToken);
+        try
+        {
+            var result = await Auth.CurrentUser.LinkWithCredentialAsync(credential);
+            onSuccess?.Invoke(result.User);
+        } catch (FirebaseException fe)
+        {
+            onFailure?.Invoke(fe);
+        }
+    }
 }
