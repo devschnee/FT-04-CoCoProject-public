56143757fe18ca9a576f7cdde6eef7dd10d8a3e6
JangWh <janghy95@naver.com>
2025-12-01 13:27:34 +0900

    데이터 테이블 갱신
    1. 기획에서 준 데이터로 갱신
    2. 대화, 스테이지, 화자 데이터 갱신
    3. 영상 추가

--- Assets/_Proj/Scripts/Stage/StageManager.cs
+++ Assets/_Proj/Scripts/Stage/StageManager.cs
@@ -184,6 +184,10 @@ public class StageManager : MonoBehaviour, IStageManager, IQuestBehaviour
             yield return PlayCutscene(data_end_cutscene);
         }
 
+        StageUIManager.Instance.stageIdInformation.stageIdInfo = currentStageId;
+        var fp = StageUIManager.Instance.FadePanel;
+        fp.SetActive(true);
+
         // 메인씬으로 이동
         //Todo : 챕터에 따라 스테이지 선택화면 분기
         SceneManager.LoadScene("Main");
