0fabea8cfcd70e2e80b72dec8a62d2196a8b285f
이강욱 <kangwook.yi@gmail.com>
2025-12-03 17:52:06 +0900

    친구 로비에서 좋아요 보내기 가능, 친구의 변동상황 친구 로비에서 반영됨.

--- Assets/_Proj/Scripts/FriendLobby/FriendLobbyManager.cs
+++ Assets/_Proj/Scripts/FriendLobby/FriendLobbyManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FriendLobbyManager : MonoBehaviour
@@ -7,6 +9,10 @@ public class FriendLobbyManager : MonoBehaviour
     public UserData Friend { get; private set;  }
     public string Uid { get; private set; }
     public static FriendLobbyManager Instance { get; private set; }
+
+    public float updateInterval = 3f;
+    public float currentTimer;
+    public bool isRecentlyUpdated;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -19,6 +25,30 @@ public class FriendLobbyManager : MonoBehaviour
     
     }
 
+
+    private void Update()
+    {
+        currentTimer += Time.deltaTime;
+        if (currentTimer > 1f && isRecentlyUpdated)
+        {
+            isRecentlyUpdated = false;
+        }
+        if (currentTimer > updateInterval)
+        {
+            currentTimer = 0f;
+            isRecentlyUpdated = true;
+            HelpMaintain();
+        }
+    }
+
+    public async void HelpMaintain()
+    {
+        var friendMaster = await FirebaseManager.Instance.DownloadUserDataCategory(Uid, UserDataDirtyFlag.Master) as UserData.Master;
+        Friend.master.totalLikes = friendMaster.totalLikes;
+        Friend.master.profile = friendMaster.profile;
+        Friend.master.onMasterUpdate?.Invoke();
+    }
+
     public void Init(UserData friend, string friendUid)
     {
         Friend = friend;
@@ -26,4 +56,5 @@ public class FriendLobbyManager : MonoBehaviour
         FriendLobby = friend.lobby;
         Uid = friendUid;
     }
+
 }
